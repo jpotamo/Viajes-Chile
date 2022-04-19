f8b4bdcf82fdccbd173138e6fa0c32041c208b81
bflores <bflores@chile.cl>
2022-04-19 18:54:16 -0400

    Actualiza Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,14 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
+/*modal*/
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
     
     
     
